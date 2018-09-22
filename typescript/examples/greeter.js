1256a087ccf0dfe5453c748ba52ff320a77a8f7c
kurtiskubal <kurtiskubal@gmail.com>
2018-09-22 12:45:11 -0600

    added examples and modified files to add bootstrap

---
+++ typescript/examples/greeter.js
@@ -0,0 +1,16 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = new Student("Jane", "M.", "User");
+// let user = { firstName: "Jane", lastName: "User"};
+// let user = [1, 2, 3];
+document.body.innerHTML = greeter(user);
